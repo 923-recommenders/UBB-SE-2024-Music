edb89abb4f721fdcb301e88dbd22e8f27d70f7d0
OCTAVIAN-MIHAIL GHEORGHE <octavian.gheorghe@stud.ubbcluj.ro>
2024-05-27 01:13:46 +0300

    commit for adding top5songs to the project

---
+++ Controllers/Top5SongsController.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UBB_SE_2024_Music.Services;
+
+namespace UBB_SE_2024_Music.Controllers
+{
+    public class Top5SongsController : Controller
+    {
+        private readonly IRecapService recapService;
+
+        public Top5SongsController(IRecapService rrecapService)
+        {
+            recapService = rrecapService;
+        }
+
+        // /Top5Songs?UserId=
+        public async Task<IActionResult> Index(int userId)
+        {
+            var topFiveSongs = await recapService.GetTheTop5MostListenedSongs(userId);
+            return View(topFiveSongs);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using UBB_SE_2024_Music.Data;
+using UBB_SE_2024_Music.Repositories;
+using UBB_SE_2024_Music.Services;
 
 namespace UBB_SE_2024_Music
 {
@@ -19,6 +21,10 @@ namespace UBB_SE_2024_Music
             builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddScoped<IRecapService, RecapService>();
+            builder.Services.AddScoped<ISongBasicDetailsRepository, SongBasicDetailsRepository>();
+            builder.Services.AddScoped<IUserPlaybackBehaviourRepository, UserPlaybackBehaviourRepository>();
 
             var app = builder.Build();
 
