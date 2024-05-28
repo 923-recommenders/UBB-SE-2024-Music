eb1436b78a0fdd7e6e84aed3fa5b5ffa6df6c2cf
MogaDenis <moga_denis@yahoo.com>
2024-05-28 23:52:34 +0300

    Worked on Playlist and Creation views, changed the starting page so you can navigate to song creation and playlists, also I added a prompt telling the user to log in prior to using the app

--- Controllers/CreationController.cs
+++ Controllers/CreationController.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using UBB_SE_2024_Music.Models;
 using UBB_SE_2024_Music.Services;
@@ -6,6 +7,7 @@ using UBB_SE_2024_Music.Services.Interfaces;
 
 namespace UBB_SE_2024_Music.Controllers
 {
+    [Authorize]
     public class CreationController : Controller
     {
         private readonly ICreationService creationService;
---
+++ DTO/PlaylistDetailsViewModel.cs
@@ -0,0 +1,10 @@
+﻿using UBB_SE_2024_Music.Models;
+
+namespace UBB_SE_2024_Music.DTO
+{
+    public class PlaylistDetailsViewModel
+    {
+        public Playlist Playlist { get; set; } = null!;
+        public IEnumerable<Song> Songs { get; set; } = null!;
+    }
+}
