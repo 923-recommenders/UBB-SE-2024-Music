57f33a54319a961ea0c8af1f4125d8a36382c275
Carla021 <cmirea03@gmail.com>
2024-05-29 13:29:25 +0300

    added-songController

---
+++ DTO/SongDeleteViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace UBB_SE_2024_Music.DTO
+{
+    public class SongDeleteViewModel
+    {
+        public int PlaylistId { get; set; }
+        public int SongId { get; set; }
+        public string Name { get; set; }
+        public string ArtistName { get; set; }
+    }
+}
