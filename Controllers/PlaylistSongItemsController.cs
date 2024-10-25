d9ccdc16978ade5f44b97de1c66bbf7861e62e88
915-Micu-AlexiaClaudia <alexiamicu@gmail.com>
2024-10-25 12:07:43 +0300

    Revert "Button from playlist redirects to songs."
    This reverts commit 5e5de7bbb0956bc7674bd5751ad89469a0d5228f.

--- Controllers/PlaylistSongItemsController.cs
+++ Controllers/PlaylistSongItemsController.cs
@@ -18,12 +18,6 @@ namespace UBB_SE_2024_Music.Controllers
             _playlistSongItemService = playlistSongItemService ?? throw new ArgumentNullException(nameof(playlistSongItemService));
         }
 
-        public async Task<IActionResult> Index(int id)
-        {
-            var playlistSongs = await _playlistSongItemService.GetSongsByPlaylistId(id);
-            return View(playlistSongs);
-        }
-
         public async Task<IActionResult> SongsManagement(int id)
         {
             try
